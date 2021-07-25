1e1f2eb1e013638bb75a988108113fa94ba33602
Matin <matin.parsapour.iam@gmail.com>
2021-07-25 11:16:06 +0430

    get username and password of user for login

--- src/Service/CustomerService.java
+++ src/Service/CustomerService.java
@@ -1,4 +1,14 @@
 package Service;
 
+import java.util.Scanner;
+
 public class CustomerService {
+    static Scanner input = new Scanner(System.in);
+    public void logIn(){
+        System.out.println("<><><><><><> log in <><><><><><>");
+        System.out.println("---------- user name -----------");
+        String username = input.next();
+        System.out.println("----------- password -----------");
+        int password = input.nextInt();
+    }
 }
