1154a814f8830fc7fcd5bdb350e06936636c23c9
Matin <matin.parsapour.iam@gmail.com>
2021-07-25 08:37:29 +0430

    Create customerTable method to create customer table in database

--- src/repository/TableRepository.java
+++ src/repository/TableRepository.java
@@ -1,4 +1,17 @@
 package repository;
 
+import java.sql.*;
+
 public class TableRepository {
+    public void  customerTable() throws SQLException {
+        Connection connection = DriverManager.getConnection("\"jdbc:mysql://localhost:3306/website\", \"root\", \"Mm1234!@#$\"");
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS `store`.`new_table` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `username` VARCHAR(100) NOT NULL,\n" +
+                "  `password` VARCHAR(11) NOT NULL,\n" +
+                "  `balance` INT NOT NULL DEFAULT 0,\n" +
+                "  PRIMARY KEY (`id`));");
+    }
 }
