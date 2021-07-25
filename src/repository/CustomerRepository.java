6367decfd31794aa6ae3d1efcd99da7917de7741
Matin <matin.parsapour.iam@gmail.com>
2021-07-25 11:22:05 +0430

    create check username and password to check if user wrote correct username and password

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import java.sql.*;
+
 public class CustomerRepository {
+    public boolean checkUsernameAndPassword(String username, String password) throws SQLException {
+        boolean usernameIsOk = false;
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/website", "root", "Mm1234!@#$");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM customer");
+        while(resultSet.next()){
+            if(resultSet.getString("username").equals(username) && resultSet.getString("password").equals(password)){
+                usernameIsOk = true;
+            }
+        }
+        return usernameIsOk;
+    }
 }
