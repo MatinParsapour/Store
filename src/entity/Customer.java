2933f1ae81dac16f15cc1a9e37cdf4e0a6b2a561
Matin <matin.parsapour.iam@gmail.com>
2021-07-25 10:43:08 +0430

    create customer property and methods

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,4 +1,40 @@
 package entity;
 
 public class Customer {
+    private static String name;
+    private static String username;
+    private static String password;
+    private static int balance;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Customer.name = name;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        Customer.username = username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        Customer.password = password;
+    }
+
+    public static int getBalance() {
+        return balance;
+    }
+
+    public static void setBalance(int balance) {
+        Customer.balance = balance;
+    }
 }
