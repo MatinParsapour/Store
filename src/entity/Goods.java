78ab560a38a80db48168464610a4662518fb618d
Matin <matin.parsapour.iam@gmail.com>
2021-07-25 10:43:45 +0430

    create goods property and methods

--- src/entity/Goods.java
+++ src/entity/Goods.java
@@ -1,4 +1,13 @@
 package entity;
 
 public class Goods {
+    private static int goodsId;
+
+    public static int getGoodsId() {
+        return goodsId;
+    }
+
+    public static void setGoodsId(int goodsId) {
+        Goods.goodsId = goodsId;
+    }
 }
