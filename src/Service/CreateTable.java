707d8fe2950435b53469e309d721df95691847fd
Matin <matin.parsapour.iam@gmail.com>
2021-07-26 21:18:23 +0430

    upadte create table class now admin can verify and suspend and unsuspend someone

--- src/Service/CreateTable.java
+++ src/Service/CreateTable.java
@@ -6,13 +6,14 @@ public class CreateTable {
     public void  customerTable() throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/store", "root", "Mm1234!@#$");
         Statement statement = connection.createStatement();
-        statement.execute("CREATE TABLE IF NOT EXISTS `store`.`customer` (\n" +
+        statement.execute("CREATE TABLE `store`.`customer` (\n" +
                 "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
                 "  `name` VARCHAR(45) NOT NULL,\n" +
                 "  `username` VARCHAR(100) NOT NULL,\n" +
                 "  `password` VARCHAR(11) NOT NULL,\n" +
                 "  `balance` INT NOT NULL DEFAULT 0,\n" +
-                "  PRIMARY KEY (`id`));");
+                "  `verify` INT NOT NULL DEFAULT 0,\n" +
+                "  PRIMARY KEY (`id`));\n");
     }
     public void goodsTable()throws SQLException{
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/store", "root", "Mm1234!@#$");
