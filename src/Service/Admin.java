9d3ead5a89c90d1e2cb08725d63a0d3c17959b05
Matin <matin.parsapour.iam@gmail.com>
2021-07-26 16:21:37 +0430

    primary design for admin to log in

--- src/Service/Admin.java
+++ src/Service/Admin.java
@@ -1,4 +1,15 @@
 package Service;
 
+import java.util.Scanner;
+
 public class Admin {
+    static Scanner input = new Scanner(System.in);
+    public void adminLogIn(){
+        System.out.println("------ log in admin ------");
+        System.out.println("-------- username --------");
+        String username = input.next();
+        System.out.println("-------- password --------");
+        String password = input.next();
+
+    }
 }
